340c12f7ab5ecc901f449cdfce8163d6238d814e
kunkun <kurnia.wirawan@gmail.com>
2013-06-14 00:39:27 +0700

    init

---
+++ WcfServiceContract/TaskListService.svc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Activation;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceContract
+{
+    [ServiceContract]
+    [AspNetCompatibilityRequirements(RequirementsMode = AspNetCompatibilityRequirementsMode.Allowed)]
+    public class TaskListService 
+    {
+        [OperationContract]
+        [WebGet(ResponseFormat = WebMessageFormat.Json)]
+        public List<TaskList> GetTaskLists()
+        {
+            using (var entity = new PushDataEntities())
+            {
+                var result = from data in entity.TaskLists
+                             select data;
+                return result.Take(50).ToList();
+            }
+        }
+    }
+}
